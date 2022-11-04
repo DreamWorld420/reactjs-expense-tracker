a63254e86fd0627ba6391ebbfee9a3412e4974f4
DreamWorld420 <dreamworld420@protonmail.com>
2022-11-04 17:52:48 +0330

    feat: add more pages

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,14 +1,68 @@
 import React from "react";
+import { useLocation, type Location } from "react-router-dom";
+import {
+	AiFillHome,
+	AiFillWallet,
+	AiFillPlusCircle,
+	AiFillPieChart,
+	AiFillSetting,
+} from "react-icons/ai";
 
 const Navigation: React.FC = () => {
+	const location = useLocation();
+
+	const calcOpacity = (path: string) => {
+		return location.pathname === `/${path}` ? 1 : undefined;
+	};
+
 	return (
 		<nav className="fixed bottom-0 left-0 h-16 w-screen">
-			<ul>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
+			<ul className="flex flex-row items-center justify-between px-8">
+				<li className="flex flex-col justify-center">
+					<button
+						className="opacity-30 transition-opacity hover:opacity-100"
+						style={{
+							opacity: calcOpacity(""),
+						}}
+					>
+						<AiFillHome size={25} />
+					</button>
+				</li>
+				<li className="flex flex-col justify-center">
+					<button
+						className="opacity-30 transition-opacity hover:opacity-100"
+						style={{
+							opacity: calcOpacity("wallet"),
+						}}
+					>
+						<AiFillWallet size={25} />
+					</button>
+				</li>
+				<li className="flex flex-col justify-center">
+					<button className="flex flex-col justify-center drop-shadow">
+						<AiFillPlusCircle size={45} />
+					</button>
+				</li>
+				<li className="flex flex-col justify-center">
+					<button
+						className="opacity-30 transition-opacity hover:opacity-100"
+						style={{
+							opacity: calcOpacity("chart"),
+						}}
+					>
+						<AiFillPieChart size={25} />
+					</button>
+				</li>
+				<li className="flex flex-col justify-center">
+					<button
+						className="opacity-30 transition-opacity hover:opacity-100"
+						style={{
+							opacity: calcOpacity("settings"),
+						}}
+					>
+						<AiFillSetting size={25} />
+					</button>
+				</li>
 			</ul>
 		</nav>
 	);
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+import Chart from "./pages/Chart";
 import Root from "./pages/Root";
+import Settings from "./pages/Settings";
+import Wallet from "./pages/Wallet";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Root />,
+		children: [
+			{
+				path: "wallet",
+				element: <Wallet />,
+			},
+			{
+				path: "chart",
+				element: <Chart />,
+			},
+			{
+				path: "settings",
+				element: <Settings />,
+			},
+		],
 	},
 ]);
 
