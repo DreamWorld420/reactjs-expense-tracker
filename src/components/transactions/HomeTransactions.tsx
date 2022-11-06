20225c48db637f92130ede39fb3fe348545c6a10
DreamWorld420 <dreamworld420@protonmail.com>
2022-11-06 20:59:34 +0330

    refactor: refactor TransactionCard

--- src/components/transactions/HomeTransactions.tsx
+++ src/components/transactions/HomeTransactions.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { useAppSelector } from "app/hooks";
-import { TransactionType } from "types";
-import currencyFormatter from "utils/currencyFormatter";
+import TransactionCard from "./TransactionCard";
 
 const HomeTransactions: React.FC = () => {
 	const lastTenTransactions = useAppSelector((state) =>
 		state.transactions.value.slice(-10)
 	);
 
+	// No transaction available
 	if (!lastTenTransactions.length) {
 		return (
 			<div className="shadow-4xl flex grow flex-col items-center justify-center rounded bg-white font-robotoMono leading-loose drop-shadow-2xl">
@@ -21,32 +21,13 @@ const HomeTransactions: React.FC = () => {
 	// that it grows and fill parent but overflows
 	return (
 		<div className="flex h-1 grow flex-col gap-y-2 overflow-auto">
-			{lastTenTransactions.map(({ value, date, name, type }) => {
+			{lastTenTransactions.map((transaction) => {
 				return (
-					<div
-						key={`${value}${date}`}
-						className="flex  items-center justify-between  rounded bg-white py-2 px-4 font-robotoMono"
+					<React.Fragment
+						key={`${transaction.value}${transaction.date}`}
 					>
-						<section className="flex flex-col">
-							<h3 className="text-sm font-semibold">{name}</h3>
-							<p className="text-xs text-gray-400">
-								{new Date(date).toLocaleDateString("en-US", {
-									year: "numeric",
-									month: "long",
-									day: "2-digit",
-									hour: "numeric",
-									minute: "numeric",
-								})}
-							</p>
-						</section>
-						<p className="text-sm">
-							{currencyFormatter(
-								Number(type) === TransactionType.EXPENSE
-									? -value
-									: value
-							)}
-						</p>
-					</div>
+						<TransactionCard transaction={transaction} />
+					</React.Fragment>
 				);
 			})}
 		</div>
---
+++ src/components/transactions/TransactionCard.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Transaction, TransactionType } from "types";
+import currencyFormatter from "utils/currencyFormatter";
+
+interface IProps {
+	transaction: Transaction;
+}
+
+const TransactionCard: React.FC<IProps> = ({
+	transaction: { value, date, name, type },
+}) => {
+	return (
+		<div className="flex  items-center justify-between  rounded bg-white py-2 px-4 font-robotoMono">
+			<section className="flex flex-col">
+				<h3 className="text-sm font-semibold">{name}</h3>
+				<p className="text-xs text-gray-400">
+					{new Date(date).toLocaleDateString("en-US", {
+						year: "numeric",
+						month: "long",
+						day: "2-digit",
+						hour: "numeric",
+						minute: "numeric",
+					})}
+				</p>
+			</section>
+			<p className="text-sm">
+				{currencyFormatter(
+					Number(type) === TransactionType.EXPENSE ? -value : value
+				)}
+			</p>
+		</div>
+	);
+};
+
+export default TransactionCard;
