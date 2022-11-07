93efdb27039d3d5947c23b18b10a121e368c0449
DreamWorld420 <dreamworld420@protonmail.com>
2022-11-07 03:41:44 +0330

    feat: implement persisted redux store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import thunk from "redux-thunk";
 
 import balanceReducer from "@Features/balance/balanceSlice";
 import transactionsReducer from "@Features/transactions/TransactionsSlice";
 
-export const store = configureStore({
-	reducer: {
-		balance: balanceReducer,
-		transactions: transactionsReducer,
-	},
+const combinedReducers = combineReducers({
+	balance: balanceReducer,
+	transactions: transactionsReducer,
 });
 
+const persistConfig = {
+	key: "root",
+	storage,
+};
+
+const persistedReducers = persistReducer(persistConfig, combinedReducers);
+
+export const store = configureStore({
+	reducer: persistedReducers,
+	middleware: [thunk],
+});
+
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "@App/store";
+import { persistor, store } from "@App/store";
 
 import router from "@Pages/router";
 
@@ -12,7 +13,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<RouterProvider router={router} />
+			<PersistGate loading={null} persistor={persistor}>
+				<RouterProvider router={router} />
+			</PersistGate>
 		</Provider>
 	</React.StrictMode>
 );
